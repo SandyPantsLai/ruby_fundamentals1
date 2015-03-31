f05abec97159bc65124bc753eb85c36fa52f04c5
Sandy Lai <sandywslai@gmail.com>
2015-03-31 14:30:37 -0400

    Added question for user's age and output possible years they were born in Exercise 3.

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,21 @@
+#Ask for user's name and greet
+
 puts "What is your name?"
 
 name = gets.chomp.capitalize
 
 puts "Hi #{name}!"
+
+#Ask for age and outputs the possible years they were born in.  
+
+this_year = Time.new
+
+puts "How old are you?"
+
+age = gets.chomp.to_i
+
+birthyear_high = (this_year.year - age)
+birthyear_low = (this_year.year - age - 1)
+
+puts "If you already had a birthday this year, you were born in #{birthyear_high}.  If you did not have your birthday yet this year, you were born in #{birthyear_low}" 
+
